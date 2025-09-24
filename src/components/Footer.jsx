0d243134c4b0a754d42fc87053a374610d35f520
Giuseppe11S <senagiuseppe11@gmail.com>
2025-09-24 17:42:24 +0200

    First component header in jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+
+const Footer = () => {
+
+  return (
+    
+    <footer>
+      <section>
+        <div className="upper-footer-label">
+          <ul>
+            <li>
+              <img src="" alt="" />
+              DIGITAL COMICS 
+            </li>
+            <li>
+              <img src="" alt="" />
+              DC MERCHANDISE
+            </li>
+            <li>
+              <img src="" alt="" />
+            </li>
+            <li>
+              <img src="" alt="" />
+            </li>
+            <li><img src="" alt="" />
+            </li>
+          </ul>
+
+        </div>
+      </section>
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+
+const Header = () => {
+  
+  return (
+    <header>
+      <div className="cnt-nav-bar">
+        <img src="./img/dc-logo.png" alt="Logo nav"/>
+          <div className="list-links">
+            <ul>
+              <li><a href="characthers">characthers</a></li>
+              <li><a href="comics">comics</a></li>
+              <li><a href="movies">movies</a></li>
+              <li><a href="tv">tv</a></li>
+              <li><a href="games">games</a></li>
+              <li><a href="colleticables">colleticables</a></li>
+              <li><a href="videos">videos</a></li>
+              <li><a href="fans">fans</a></li>
+              <li><a href="news">news</a></li>
+              <li><a href="shop">shop</a></li>
+            </ul>
+          </div>
+      </div>
+   </header>
+  )
+}
+
+export default Header
