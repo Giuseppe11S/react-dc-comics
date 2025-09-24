43c12238f4713e06b8bf5776a2c2745be604c5a2
Giuseppe11S <senagiuseppe11@gmail.com>
2025-09-24 18:18:18 +0200

    Footer sections completed, upper,middle,bottom, component footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,6 +31,89 @@ const Footer = () => {
 
         </div>
       </section>
+
+      <section>
+        <div className="middle-footer-links">
+          <div className="footer-col-mid">
+            <h3>DC COMICS</h3>
+            <ul>
+            <li><a href="#">Characters</a></li>
+              <li><a href="#">Comics</a></li>
+              <li><a href="#">Movies</a></li>
+              <li><a href="#">TV</a></li>
+              <li><a href="#">Games</a></li>
+              <li><a href="#">Videos</a></li>
+              <li><a href="#">News</a></li>
+            </ul>
+            <h3>SHOP</h3>
+            <ul>
+              <li><a href="#">Shop DC</a></li>
+              <li><a href="#">Shop DC Collectibles</a></li>
+            </ul>
+          </div>
+
+          <div className="footer-col-mid">
+            <h3>DC</h3>
+            <ul>
+            <li><a href="#">Terms Of Use</a></li>
+              <li><a href="#">Privacy policy (New)</a></li>
+              <li><a href="#">Ad Choices</a></li>
+              <li><a href="#">Advertising</a></li>
+              <li><a href="#">Jobs</a></li>
+              <li><a href="#">Subscriptions</a></li>
+              <li><a href="#">Talent Workshops</a></li>
+              <li><a href="#">CPSC Certificates</a></li>
+              <li><a href="#">Ratings</a></li>
+              <li><a href="#">Shop Help</a></li>
+              <li><a href="#">Contact Us</a></li>
+            </ul>
+          </div>
+
+          <div className="footer-col-mid">
+            <h3>SITES</h3>
+            <ul>
+            <li><a href="#">DC</a></li>
+              <li><a href="#">MAD Magazine</a></li>
+              <li><a href="#">DC Kids</a></li>
+              <li><a href="#">DC Universe</a></li>
+              <li><a href="#">DC Power Visa</a></li>
+            </ul>
+          </div>
+          <div className="right-col-mid">
+            <img src="./img/dc-logo-bg.png" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="bottom-footer-links">
+          <div className="left-col-btm">
+            <button>SING-UP NOW!</button>
+          </div>
+          <div className="right-col-btm">
+            <ul>
+              <li>
+                <h3>FOLLOW US</h3>
+              </li>
+              <li>
+                <img src="./img/footer-facebook.png" alt="" />
+              </li>
+              <li>
+                <img src="./img/footer-twitter.png" alt="" />
+              </li>
+              <li>
+                <img src="./img/footer-youtube.png" alt="" />
+              </li>
+              <li>
+                <img src="./img/footer-pinterest.png" alt="" />
+              </li>
+              <li>
+                <img src="./img/footer-periscope.png" alt="" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </footer>
   )
 }
