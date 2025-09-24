9943c8679cf0b0d4ef5cf52db48e60777981f5f1
Giuseppe11S <senagiuseppe11@gmail.com>
2025-09-24 17:55:48 +0200

    Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,20 +8,24 @@ const Footer = () => {
         <div className="upper-footer-label">
           <ul>
             <li>
-              <img src="" alt="" />
+              <img src="./img/buy-comics-digital-comics.png" alt="Digital Comics" />
               DIGITAL COMICS 
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="./img/buy-comics-merchandise.png" alt="" />
               DC MERCHANDISE
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="./img/buy-comics-subscriptions.png" alt="" />
+              SUBSCRIPTION
             </li>
             <li>
-              <img src="" alt="" />
+              <img src="./img/buy-comics-shop-locator.png" alt="" />
+              COSMIC SHOP LOCATOR
             </li>
-            <li><img src="" alt="" />
+            <li>
+            
+            DC POWER VISA
             </li>
           </ul>
 
@@ -30,3 +34,5 @@ const Footer = () => {
     </footer>
   )
 }
+
+export default Footer
